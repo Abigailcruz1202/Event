4f4c44bc168f5999bcfcd4b21daf72fe472d4f22
Viviana Abigail Cruz <cruzabi994@gmail.com>
2021-09-26 22:22:41 -0300

    actualizado

--- src/components/FormUsers/FormUsers.jsx
+++ src/components/FormUsers/FormUsers.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import styles from "../FormPromoter/Forms.module.css";
+import styles from "../FormPromoter/Form.module.css";
 
 export function Validate(input) {
   let errors = {};
